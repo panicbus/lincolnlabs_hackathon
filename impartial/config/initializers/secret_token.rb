47a46bc490e52f012419c6791df074fd589251af
panicbus <elementalair@gmail.com>
2014-07-18 23:29:42 -0700

    initial commit

---
+++ impartial/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impartial::Application.config.secret_key_base = 'c5b6f23737ca0b09342848cab199bbe2d5a27c8063f227e8122bedebe37eeef5bd2fb6f52fc13ea6b97c8bea5c8354a735d3910b4d646f0a379efe7d263d3633'
